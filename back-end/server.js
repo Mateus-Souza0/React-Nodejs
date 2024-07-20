6fb4924b31656e8549530cc04a4219cf67e127ad
Mateus Souza <mateussouza00@hotmail.com>
2024-07-19 21:12:45 -0300

    Adicionando UPDATE e ajustando método get e post

--- back-end/server.js
+++ back-end/server.js
@@ -1,24 +1,49 @@
 
-import express, {request} from 'express'
+import express, { request } from 'express'
+import { PrismaClient } from '@prisma/client'
 
 
-
+const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-const users = []
 
-app.post('/users' , (req, res) => {
-    
-    users.push(req.body)
 
-    res.status(201)
+app.post('/users', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name
+        }
+    })
+
+    res.status(201).json(req.body)
 })
 
-app.get('/users' , (req, res) => {
+app.get('/users', async (req, res) => {
 
+    const users = await prisma.user.findMany()
     res.status(200).json(users)
 
+})
+
+app.put('/users/:id', async (req, res) => {
+
+    await prisma.user.update({
+
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            email: req.body.email
+            
+        }
+    })
+
+
+    res.status(201).json(req.body)
 })
 
 
