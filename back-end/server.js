7be0af469f4033b7bf595601c23c795e207b8444
Mateus Souza <mateussouza00@hotmail.com>
2024-07-24 12:16:54 -0300

    Instalando Axios e Cors

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,13 @@
 
 import express, { request } from 'express'
 import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
 
 
 const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 
